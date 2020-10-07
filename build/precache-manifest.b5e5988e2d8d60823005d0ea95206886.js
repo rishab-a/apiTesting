885939e63b3cd08d4145f7a92ace7add9e3aeb87
ritu.s <ritu.s@endurance.com>
2020-10-08 03:41:08 +0530

    prepare to deploy

---
+++ build/precache-manifest.b5e5988e2d8d60823005d0ea95206886.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47daa9f38459e9ceebe542838ef7a379",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d48fdee7e1ac6717797",
+    "url": "/static/css/main.6e46e629.chunk.css"
+  },
+  {
+    "revision": "16833a58e2b77846bd00",
+    "url": "/static/js/2.5aceed8c.chunk.js"
+  },
+  {
+    "revision": "b26b751f5d83e5c0f8b2fdd2c4b93e2b",
+    "url": "/static/js/2.5aceed8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d48fdee7e1ac6717797",
+    "url": "/static/js/main.fd81144f.chunk.js"
+  },
+  {
+    "revision": "0337553045cf68adb5ce",
+    "url": "/static/js/runtime-main.a6a7a6b5.js"
+  }
+]);
